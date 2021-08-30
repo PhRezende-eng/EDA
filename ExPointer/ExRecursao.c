d40f0005db647d22986374caf9f8e42b6fc09ae1
PhRezende-eng <ph.hr.001@gmail.com>
2021-08-30 18:14:53 -0300

    just done a exercise

--- ExPointer/ExRecursao.c
+++ ExPointer/ExRecursao.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double potenciacao(int a, int b){
-    if(b == 0)
+double potenciacao(int a, int b)
+{
+    if (b == 0)
         return 1;
     else
-        return a * potenciacao(a, b-1);
-
+        return a * potenciacao(a, b - 1);
 }
 
-int main(){
-    int a,b;
+int main()
+{
+    int a, b;
     double res;
-    printf("Digite a base: ");
+    // printf("Digite a base: ");
     scanf("%d", &a);
-    printf("Digite o expoente: ");
+    // printf("Digite o expoente: ");
     scanf("%d", &b);
 
-    if (b < 0)
-        res = 1.0 / potenciacao (a, -b);
-    else
-        res = potenciacao (a, b);
-    
-    printf("%d elevado a %d = %.1f\n", a, b, res);
+    if (a == 0 && b <= 0)
+        printf("indefinido\n");
+    else{
+        if (b < 0)
+            res = 1.0 / potenciacao(a, -b);
+        else
+            res = potenciacao(a, b);
+        printf("%.3f\n", res);
+    }
+
     return 0;
 }
