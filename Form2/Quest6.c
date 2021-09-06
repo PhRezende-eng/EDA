1435d43bc267c55907b4029b671f89f54b95c089
PhRezende-eng <ph.hr.001@gmail.com>
2021-09-05 21:57:48 -0300

    -minor fix

--- Form2/Quest6.c
+++ Form2/Quest6.c
@@ -6,16 +6,18 @@ long int fibonacci(int n)
 {
     if (n == 1 || n == 2)
     {
-        n == 1 ? printf("1\n") : printf("1\n1\n");
+        // n == 1 ? printf("1\n") : printf("1\n1\n");
         return 1;
     }
     if (vetor[n] == 0)
-        vetor[n] = fibonacci(n - 1) + fibonacci(n - 2);
+    {
+        vetor[n] = fibonacci(n - 2) + fibonacci(n - 1);
+    }
+
+    // printf("%ld\n", vetor[n]);
     return vetor[n];
 }
-
-int main()
-{
-    fibonacci(11);
-    return 0;
-}
+// int main(){
+//     fibonacci(11);
+// return 0;
+// }
