6a5035af497a9d051693f4f87feca3334b7046a5
PhRezende-eng <ph.hr.001@gmail.com>
2021-09-14 18:08:26 -0300

    create a logic that found the largest int of a list

---
+++ Ordenation/LargestInt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i, list[5] = {4, 2, 3, 5, 9};
+    int maior = list[0];
+    printf("%d\n", maior);
+    for (i = 1; i < 5; i++)
+    {
+        if (list[i] > maior)
+            maior = list[i];
+    }
+    printf("%d\n", maior);
+    return 0;
+}
